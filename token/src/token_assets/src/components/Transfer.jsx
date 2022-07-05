1adbc9d28c019790be5c255a06e6f5a4a9a42a67
nkusikevin <nkusikvnhart@gmail.com>
2022-07-05 19:21:33 +0200

    transfer amount front-end integration

--- token/src/token_assets/src/components/Transfer.jsx
+++ token/src/token_assets/src/components/Transfer.jsx
@@ -1,44 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { token } from "../../../declarations/token";
 
 function Transfer() {
-  
-  async function handleClick() {
-    
-  }
+	const [amount, setAmount] = useState(0);
+	const [to, setTo] = useState("");
 
-  return (
-    <div className="window white">
-      <div className="transfer">
-        <fieldset>
-          <legend>To Account:</legend>
-          <ul>
-            <li>
-              <input
-                type="text"
-                id="transfer-to-id"
-              />
-            </li>
-          </ul>
-        </fieldset>
-        <fieldset>
-          <legend>Amount:</legend>
-          <ul>
-            <li>
-              <input
-                type="number"
-                id="amount"
-              />
-            </li>
-          </ul>
-        </fieldset>
-        <p className="trade-buttons">
-          <button id="btn-transfer" onClick={handleClick} >
-            Transfer
-          </button>
-        </p>
-      </div>
-    </div>
-  );
+	async function handleClick() {
+		await token.transfer(to, amount);
+	}
+
+	return (
+		<div className='window white'>
+			<div className='transfer'>
+				<fieldset>
+					<legend>To Account:</legend>
+					<ul>
+						<li>
+							<input
+								type='text'
+								id='transfer-to-id'
+								value={to}
+								onChange={(e) => setTo(e.target.value)}
+							/>
+						</li>
+					</ul>
+				</fieldset>
+				<fieldset>
+					<legend>Amount:</legend>
+					<ul>
+						<li>
+							<input
+								type='number'
+								id='amount'
+								value={amount}
+								onChange={(e) => setAmount(e.target.value)}
+							/>
+						</li>
+					</ul>
+				</fieldset>
+				<p className='trade-buttons'>
+					<button id='btn-transfer' onClick={handleClick}>
+						Transfer
+					</button>
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default Transfer;
