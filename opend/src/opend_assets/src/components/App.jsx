a5a82fec64c042e7dbb52fb06bf6bd21127e0936
nkusikevin <nkusikvnhart@gmail.com>
2022-07-12 10:19:04 +0200

    loading nft on front-end

--- opend/src/opend_assets/src/components/App.jsx
+++ opend/src/opend_assets/src/components/App.jsx
@@ -3,15 +3,18 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
+import Item from "./Item";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <img className="bottom-space" src={homeImage} />
-      <Footer />
-    </div>
-  );
+function App(props) {
+	const NFTID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
+	return (
+		<div className='App'>
+			<Header />
+			<Item id={NFTID} />
+			{/* <img className="bottom-space" src={homeImage} /> */}
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- opend/src/opend_assets/src/components/Item.jsx
+++ opend/src/opend_assets/src/components/Item.jsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
+import { Principal } from "@dfinity/principal";
 
-function Item() {
-  return (
-    <div className="disGrid-item">
-      <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
-        <img
-          className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
-          src={logo}
-        />
-        <div className="disCardContent-root">
-          <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            CryptoDunks #312<span className="purple-text"></span>
-          </h2>
-          <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
-            Owner: sdfsdf-erwerv-sdf
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+function Item(props) {
+	//rrkah-fqaaa-aaaaa-aaaaq-cai
+	const [name, setName] = useState("");
+
+	const id = Principal.fromText(props.id);
+	const localHost = "http://localhost:8080/";
+	const agent = new HttpAgent({ host: localHost });
+
+	async function loadNFT() {
+		const NFTActor = await Actor.createActor(idlFactory, {
+			agent,
+			canisterId: id,
+		});
+
+		const name = await NFTActor.getName();
+		setName(name);
+	}
+
+	useEffect(() => {
+		loadNFT();
+	}, []);
+
+	return (
+		<div className='disGrid-item'>
+			<div className='disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded'>
+				<img
+					className='disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img'
+					src={logo}
+				/>
+				<div className='disCardContent-root'>
+					<h2 className='disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom'>
+						{name}
+						<span className='purple-text'></span>
+					</h2>
+					<p className='disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary'>
+						Owner: sdfsdf-erwerv-sdf
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Item;
